8eee1d9067de0863e3fa2e2f21a08b6e71c0562d
Saturn-VI <112597058+Saturn-VI@users.noreply.github.com>
2024-03-24 15:31:59 -0500

    rahh i hate git

--- main.py
+++ main.py
@@ -1,119 +1,72 @@
-# Imports
-# !!! REMEMBER THAT REQUESTS NEED TO BE INSTALLED !!!
-
 from multiprocessing import Process, Value, Array, cpu_count, Lock
 from statistics import fmean, StatisticsError
+from configparser import ConfigParser
+from psutil import virtual_memory
 from requests import Session
-import os
-import sys
 from random import randint
-import time
+from os.path import isfile
 import configparser
-import psutil
+import time
+import sys
+import os
+
+# ini / config file.
+settings_file = 'settings.ini'
 
 session = Session()
-
 # uncomment for whathow (main counter)
 url, session.headers.update = "http://counter11.freecounterstat.com/private/counter.php?c=pdz4dufhlf9qlk4krksnw7twxbhlez2e&init=1711160585473", ({'referer': "https://whathow.neocities.org/"})
 # uncomment for jared site
 # url, session.headers.update = "http://www.cutercounter.com/hits.php?id=hexpacno&nd=6&style=61", ({'referer': "https://jared.nekoweb.org/"})
 
-# ini file / Config File.
-ininame = 'settings.ini'
-
-# Default Count / This should probably be changed depending to your hardware, network capabilities. 
-count = 500
-
-# Default Debug / Usally used in Development and it will be asked to be turned on if settings.ini is not present.
-debug = False
-
-# Memory Threshold / Dont change unless you know what you are doing.
-THRESHOLD = 8000 * 1024 * 1024  # 8GB / Memory of Free Space available.
-
 # Setup / Added for more automated process.
 def setup():
-    global count
     global debug
-    print(f"Checking Settings if present...")
-    file = os.path.isfile(ininame)
-    if file:
-        config = configparser.ConfigParser()
-        config.read(ininame)
-        print(f"File found, Setting Count..")
-        print(" ")
+    global count
+    global url
+
+    if isfile(settings_file):
+        config = ConfigParser()
+        config.read(settings_file)
+
         print(f"Reading GLOBAL/COUNT")
-        count = int(config.get('GLOBAL','COUNT'))
         print(f"Reading GLOBAL/DEBUG")
+
+        count = int(config.get('GLOBAL','COUNT'))
         debug = config.get('GLOBAL','DEBUG')
-        print(" ")
-        print(f"COUNT: {count} has been set")
-        print(f"DEBUG: {debug} has been set")
-        print(" ")
+
+        print(f"Count has been set to {count}.")
+        print(f"Debug has been set to {debug}.")
+ 
         memcheck()
-        debugcheck()
     else:
-        config = configparser.ConfigParser()
-        config['GLOBAL'] = {'COUNT': count,
-                            'DEBUG': debug}
+        config = ConfigParser()
+        config['GLOBAL'] = {'COUNT': 500,
+                            'DEBUG': False}
         with open(ininame, 'w') as configfile:
             config.write(configfile)
         memcheck()
-        debugcheck()
 
 def memcheck():
-    global count
-    global debug
-    mem = psutil.virtual_memory()
-    if mem.free <= THRESHOLD and count >= 500:
-        print(f"WARNING, Memory IS TOO LOW!")
-        print(f"THIS WILL RESULT IN A LOT OF LAG!!")
-        x = query_yes_no("DO YOU WISH TO CONTINUE?","no")
-        if x:
-            None
-        else:
-            quit()
+    mem = virtual_memory()
+    if (((mem.available - (10 ** 9)) / 8555000) < count):
+        print("Warning: Memory too low")
+        print("Set a lower count")
+        exit("")
+        
 
-def debugcheck():
-    global debug
-    if debug:
-        print(f"WARNING, DEBUG SET TO TRUE!")
-        print(f"THIS WILL RESULT IN A LOT OF LOGS!!")
-        x = query_yes_no("DO YOU WISH TO TURN IT OFF?","yes")
-        if x:
-            debug = False
-        else:
-            debug = True
-
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-            It must be "yes" (the default), "no" or None (meaning
-            an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == "":
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
-
+# This seems uneccesary as it's in config
+# def debugcheck():
+#     global count
+#     global debug
+#     if debug:
+#         print(f"WARNING, DEBUG SET TO TRUE!")
+#         print(f"THIS WILL RESULT IN A LOT OF LOGS!!")
+#         x = query_yes_no("DO YOU WISH TO TURN IT OFF?","no")
+#         if x:
+#             debug = True
+#         else:
+#             debug = False
 
 def process(counter, fails, sysfails, avg_time, child, lock):
     while True:
@@ -129,23 +82,18 @@ def process(counter, fails, sysfails, avg_time, child, lock):
             if (response.status_code == 200):
                 with lock:
                     counter.value += 1
-                    if debug:
-                        print(f"DEBUG | {response.status_code} | {response}")
                     avg_time[randint(0, 49)] = rtime
                     None
             # if failure, increment fail counter and randomly add response time
             elif (response.status_code != 200):
                 with lock:
                     fails.value += 1
-                    if debug:
-                        print(f"DEBUG | {response.status_code} | {response}")
                     avg_time[randint(0, 49)] = rtime
                 None
         # if exception, increment sysfails and print error
         except Exception as e:
             with lock:
-                if debug:
-                    print(e)
+                print(e)
                 sysfails.value += 1
             None
 
@@ -170,29 +118,27 @@ def main():
     # workaround so the previous printing doesn't get overwritten 
     print("100% of processes started.")
     print("Ctrl+C to end")
-    if debug == False:
-        while True:
-            # printing status, '\r' is to update newest line
-            print((f"| {(time.time() - main_start):.2f} seconds elapsed"),
-                (f"| {counter.value} succeeded"),
-                (f"| {fails.value} failed"),
-                (f"| {sysfails.value} errors"),
-                (f"| average ping: {fmean(avg_time[:4]):.3f} |"), end='\r')
+    while True:
+        # printing status, '\r' is to update newest line
+        print((f"| {(time.time() - main_start):.2f} seconds elapsed"),
+              (f"| {counter.value} succeeded"),
+              (f"| {fails.value} failed"),
+              (f"| {sysfails.value} errors"),
+              (f"| average ping: {fmean(avg_time[:4]):.3f} |"), end='\r')
 
-            time.sleep(0.08)
-    else:
-        None
+        time.sleep(0.08)
 
-# doesn't run if imported
 if (__name__ == "__main__"):
     try:
         setup()
         main()
-    # handles ctrl c without leaving orphans now
     except (KeyboardInterrupt, SystemExit):
+        # killing children
         for child in children:
             child.terminate()
         print("you killed it :(")
     except Exception as e:
+        # if someone has an issue and debug isn't on it's helpful to know that it crashed
         print(e)
         print("it died :(")
+        None
